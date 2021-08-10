f1d142cb2438d99be07b7d514dcdbc84ea0f4fea
Pratham Soni <pratham1108soni@gmail.com>
2021-08-10 13:50:43 +0530

    phoneAuthentication

---
+++ app/src/main/java/com/example/messenger/signIn/PhoneAuthentication.kt
@@ -0,0 +1,12 @@
+package com.example.messenger.signIn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.messenger.R
+
+class PhoneAuthentication : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_authentication)
+    }
+}

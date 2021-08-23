9b58234a3b5aae407b240069372bcd6e02b0ad80
Pratham Soni <pratham1108soni@gmail.com>
2021-08-23 07:20:39 +0530

    userDao updated

--- app/src/main/java/com/example/messenger/dao/UserDao.kt
+++ app/src/main/java/com/example/messenger/dao/UserDao.kt
@@ -2,19 +2,12 @@ package com.example.messenger.dao
 
 import com.example.messenger.models.User
 import com.google.firebase.database.FirebaseDatabase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class UserDao {
     private val database = FirebaseDatabase.getInstance()
     private val userCollection = database.reference.child("users")
 
-    fun addUser(user: User?){
-        user?.let {
-            GlobalScope.launch(Dispatchers.IO){
-                userCollection.child(user.uid).setValue(user)
-            }
-        }
+    fun addUser(user: User){
+        userCollection.child(user.uid).setValue(user)
     }
 }

dfde8e8697cd9e42c9ff1a034a11bc1289889405
Pratham Soni <pratham1108soni@gmail.com>
2021-08-24 14:24:11 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/messenger/MainActivity.kt
+++ app/src/main/java/com/example/messenger/MainActivity.kt
@@ -2,10 +2,75 @@ package com.example.messenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.messenger.adapters.UserAdapter
+import com.example.messenger.dao.UserDao
+import com.example.messenger.models.User
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var users: ArrayList<User>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        users = ArrayList()
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        userAdapter = UserAdapter(users)
+        recyclerView.adapter = userAdapter
+
+        setUpUserRecycler()
+
+    }
+
+    private fun setUpUserRecycler() {
+        val userDao = UserDao()
+        userDao.userCollection.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                users.clear()
+                for (snapshot1 in snapshot.children){
+                    val user = snapshot1.getValue(User::class.java)
+                    user?.let { users.add(it) }
+                }
+                userAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.search -> {
+                Toast.makeText(applicationContext,"Search", Toast.LENGTH_SHORT).show()
+            }
+            R.id.setting -> {
+                Toast.makeText(applicationContext,"Setting", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
